8839bf737874aac720c507030e39b2d88c5682f7
vladimir1299 <vladimir1299@abv.bg>
2021-08-01 17:05:10 +0300

    Initialize

---
+++ DIY.Castle/DIY.Castle.Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using DIY.Castle.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIY.Castle.Web.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ DIY.Castle/DIY.Castle.Data/Models/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace DIY.Castle.Data.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public ApplicationUser()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+    }
+}
