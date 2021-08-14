24c5650a5048ed17f4f38b86941bd383c723de7f
vladimir1299 <vladimir1299@abv.bg>
2021-08-14 19:27:01 +0300

    Add models

--- DIY.Castle/DIY.Castle.Web/Controllers/CartController.cs
+++ DIY.Castle/DIY.Castle.Web/Controllers/CartController.cs
@@ -1,6 +1,10 @@
-﻿using DIY.Castle.Web.Helpers;
+﻿using DIY.Castle.Data.Models;
+using DIY.Castle.Web.Helpers;
+using DIY.Castle.Web.Models;
+using DIY.Castle.Web.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DIY.Castle.Web.Controllers
 {
@@ -10,52 +14,77 @@ namespace DIY.Castle.Web.Controllers
         [Route("index")]
         public IActionResult Index()
         {
-            var cart = SessionHelper.GetObjectFromJson<List<Item>>(HttpContext.Session, "cart");
-            ViewBag.cart = cart;
-            ViewBag.total = cart.Sum(item => item.Product.Price * item.Quantity);
-            return View();
+            var cart = SessionHelper.GetObjectFromJson<List<ProductCartModel>>(HttpContext.Session, "cart");
+
+            var viewModel = new CartViewModel
+            {
+                Cart = cart,
+                TotalPrice = cart.Sum(p => p.Product.Price * p.Quantity),
+            };
+
+            return View(viewModel);
         }
 
-        [Route("buy/{id}")]
-        public IActionResult Buy(string id)
+        [Route("buy/{id}/{quantity}")]
+        public IActionResult Buy(int id, int quantity)
         {
-            ProductModel productModel = new ProductModel();
-            if (SessionHelper.GetObjectFromJson<List<Item>>(HttpContext.Session, "cart") == null)
+            // TODO:    Create a service to get the product by Id
+            var product = new ProductModel();
+            // TODO:    Create a service to get the product by Id
+
+            if (SessionHelper.GetObjectFromJson<List<ProductCartModel>>(HttpContext.Session, "cart") == null)
             {
-                List<Item> cart = new List<Item>();
-                cart.Add(new Item { Product = productModel.find(id), Quantity = 1 });
+                var cart = new List<ProductCartModel>();
+
+                cart.Add(new ProductCartModel
+                {
+                    Product = product,
+                    Quantity = quantity
+                });
+
                 SessionHelper.SetObjectAsJson(HttpContext.Session, "cart", cart);
             }
             else
             {
-                List<Item> cart = SessionHelper.GetObjectFromJson<List<Item>>(HttpContext.Session, "cart");
-                int index = isExist(id);
+                List<ProductCartModel> cart = SessionHelper.GetObjectFromJson<List<ProductCartModel>>(HttpContext.Session, "cart");
+
+                int index = GetItemIndexInCart(id);
+
+                // If the item already exists in the cart
                 if (index != -1)
                 {
                     cart[index].Quantity++;
                 }
+                // If it doesn't
                 else
                 {
-                    cart.Add(new Item { Product = productModel.find(id), Quantity = 1 });
+                    cart.Add(new ProductCartModel
+                    {
+                        Product = product,
+                        Quantity = quantity
+                    });
                 }
+
                 SessionHelper.SetObjectAsJson(HttpContext.Session, "cart", cart);
             }
+
             return RedirectToAction("Index");
         }
 
         [Route("remove/{id}")]
-        public IActionResult Remove(string id)
+        public IActionResult Remove(int id)
         {
-            List<Item> cart = SessionHelper.GetObjectFromJson<List<Item>>(HttpContext.Session, "cart");
-            int index = isExist(id);
+            List<ProductCartModel> cart = SessionHelper.GetObjectFromJson<List<ProductCartModel>>(HttpContext.Session, "cart");
+            int index = GetItemIndexInCart(id);
             cart.RemoveAt(index);
             SessionHelper.SetObjectAsJson(HttpContext.Session, "cart", cart);
             return RedirectToAction("Index");
         }
 
-        private int isExist(string id)
+        private int GetItemIndexInCart(int id)
         {
-            List<Item> cart = SessionHelper.GetObjectFromJson<List<Item>>(HttpContext.Session, "cart");
+            List<ProductCartModel> cart = SessionHelper.GetObjectFromJson<List<ProductCartModel>>(HttpContext.Session, "cart");
+
             for (int i = 0; i < cart.Count; i++)
             {
                 if (cart[i].Product.Id.Equals(id))
