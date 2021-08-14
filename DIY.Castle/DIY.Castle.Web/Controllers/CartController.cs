632c4db45e88f717ef8fd13e16329a6654312235
vladimir1299 <vladimir1299@abv.bg>
2021-08-14 17:02:29 +0300

    Add SessionHelper and CartController

---
+++ DIY.Castle/DIY.Castle.Web/Controllers/CartController.cs
@@ -0,0 +1,70 @@
+﻿using DIY.Castle.Web.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace DIY.Castle.Web.Controllers
+{
+    [Route("cart")]
+    public class CartController : BaseController
+    {
+        [Route("index")]
+        public IActionResult Index()
+        {
+            var cart = SessionHelper.GetObjectFromJson<List<Item>>(HttpContext.Session, "cart");
+            ViewBag.cart = cart;
+            ViewBag.total = cart.Sum(item => item.Product.Price * item.Quantity);
+            return View();
+        }
+
+        [Route("buy/{id}")]
+        public IActionResult Buy(string id)
+        {
+            ProductModel productModel = new ProductModel();
+            if (SessionHelper.GetObjectFromJson<List<Item>>(HttpContext.Session, "cart") == null)
+            {
+                List<Item> cart = new List<Item>();
+                cart.Add(new Item { Product = productModel.find(id), Quantity = 1 });
+                SessionHelper.SetObjectAsJson(HttpContext.Session, "cart", cart);
+            }
+            else
+            {
+                List<Item> cart = SessionHelper.GetObjectFromJson<List<Item>>(HttpContext.Session, "cart");
+                int index = isExist(id);
+                if (index != -1)
+                {
+                    cart[index].Quantity++;
+                }
+                else
+                {
+                    cart.Add(new Item { Product = productModel.find(id), Quantity = 1 });
+                }
+                SessionHelper.SetObjectAsJson(HttpContext.Session, "cart", cart);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [Route("remove/{id}")]
+        public IActionResult Remove(string id)
+        {
+            List<Item> cart = SessionHelper.GetObjectFromJson<List<Item>>(HttpContext.Session, "cart");
+            int index = isExist(id);
+            cart.RemoveAt(index);
+            SessionHelper.SetObjectAsJson(HttpContext.Session, "cart", cart);
+            return RedirectToAction("Index");
+        }
+
+        private int isExist(string id)
+        {
+            List<Item> cart = SessionHelper.GetObjectFromJson<List<Item>>(HttpContext.Session, "cart");
+            for (int i = 0; i < cart.Count; i++)
+            {
+                if (cart[i].Product.Id.Equals(id))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+    }
+}
