e1aaea889870c5e80075cdd58cbd9c84125e2cb7
wladimir-georgiew <vladimir.georgiev.dev@gmail.com>
2021-09-25 21:18:11 +0300

    Add product details test controller/view and slickjs slider

---
+++ DIY.Castle/DIY.Castle.Web/Controllers/ProductDetailsController.cs
@@ -0,0 +1,23 @@
+﻿using DIY.Castle.Web.Services.ProductsService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DIY.Castle.Web.Controllers
+{
+    public class ProductDetailsController : BaseController
+    {
+        private readonly IProductsService productsService;
+
+        public ProductDetailsController(IProductsService productsService)
+        {
+            this.productsService = productsService;
+        }
+
+        public IActionResult Product(int id)
+        {
+            var product = this.productsService.GetProductById(id);
+            var productModel = this.productsService.GetProductModel(product);
+
+            return View(productModel);
+        }
+    }
+}
