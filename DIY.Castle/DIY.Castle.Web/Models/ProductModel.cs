3234fc857efc529fedaa8583054bf1a976e0eb8e
Nikolas Kostadinov <64860672+nikisx@users.noreply.github.com>
2021-10-21 00:48:31 +0300

    add js slider

--- DIY.Castle/DIY.Castle.Web/Models/ProductModel.cs
+++ DIY.Castle/DIY.Castle.Web/Models/ProductModel.cs
@@ -8,7 +8,7 @@ namespace DIY.Castle.Web.Models
 
         public string Name { get; set; }
 
-        public int ProductType { get; set; }
+        public string ProductType { get; set; }
 
         public decimal Price { get; set; }
 
