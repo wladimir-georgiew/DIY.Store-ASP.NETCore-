adea3979838e8ab1a5fa31833e978f52943de4c3
Vladimir Georgiev <vladimir.georgiev.dev@gmail.com>
2021-11-18 15:40:06 +0200

    Added pagination

--- DIY.Castle/DIY.Castle.Web/Controllers/CatalogController.cs
+++ DIY.Castle/DIY.Castle.Web/Controllers/CatalogController.cs
@@ -1,5 +1,8 @@
-﻿using DIY.Castle.Web.Services.ProductsService;
+﻿using DIY.Castle.Common;
+using DIY.Castle.Web.Models;
+using DIY.Castle.Web.Services.ProductsService;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,14 +17,20 @@ namespace DIY.Castle.Web.Controllers
             this.productsService = productsService;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int page = 1)
         {
-            var productModels = this.productsService.GetAllProducts().Select(x => this.productsService.GetProductModel(x)).ToList();
+            var productModels = this.productsService
+                .GetAllProducts()
+                .OrderByDescending(x => x.CreatedOn)
+                .Select(x => this.productsService.GetProductModel(x))
+                .ToList();
 
             // TODO : Make service to show all products
             // TODO : Add filtration options
             // TODO : Add pagination
-            return View(productModels);
+
+            var viewModel = PaginationList<ProductModel>.Create(productModels, page, 12);
+            return View(viewModel);
         }
     }
 }
