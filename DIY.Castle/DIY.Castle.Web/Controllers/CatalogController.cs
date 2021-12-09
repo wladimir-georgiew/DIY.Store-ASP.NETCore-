3e3a27f7c3ae9801009ffa07c9b5c067299202f1
Vladimir Georgiev <vladimir.georgiev.dev@gmail.com>
2021-12-09 03:33:35 +0200

    Upgrade the product search to show related products if the search input query is contained in the product name
    Front-end changes and bugfixes on the catalog, product details and  home pages

--- DIY.Castle/DIY.Castle.Web/Controllers/CatalogController.cs
+++ DIY.Castle/DIY.Castle.Web/Controllers/CatalogController.cs
@@ -1,7 +1,9 @@
 ﻿using DIY.Castle.Common;
+using DIY.Castle.Data.Models;
 using DIY.Castle.Web.Models;
 using DIY.Castle.Web.Services.ProductsService;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Linq;
 
 namespace DIY.Castle.Web.Controllers
@@ -15,17 +17,28 @@ namespace DIY.Castle.Web.Controllers
             this.productsService = productsService;
         }
 
-        public IActionResult Index(string filter = null, int page = 1, string searchQuery = null)
+        public IActionResult Index(string filter = null, int page = 1, string searchQuery = "")
         {
+            // Prevents from getting an error when clicking the search button with empty input ( ?searchQuery= )
+            if (searchQuery is null)
+            {
+                searchQuery = string.Empty;
+            }
+
             var isQueryNull = string.IsNullOrEmpty(searchQuery);
             var searchQueryLower = !isQueryNull ? searchQuery.ToLower() : searchQuery;
+            Func<Product, bool> filterByStartsWith = (x) => !isQueryNull ? x.Name.ToLower().StartsWith(searchQueryLower) : true;
+            Func<Product, bool> filterByStartsWithAndContains = (x) => !isQueryNull ? 
+            x.Name.ToLower().StartsWith(searchQueryLower) ||
+            x.Name.ToLower().Contains(searchQueryLower) : true;
 
             var productModels = this.productsService
                 .GetProductsByType(filter)
-                .Where(x => !isQueryNull ? x.Name.ToLower() == searchQueryLower || x.Name.ToLower().StartsWith(searchQueryLower) : true)
+                .Where(searchQuery.Length < 4 ? filterByStartsWith : filterByStartsWithAndContains)
                 .OrderByDescending(x => x.CreatedOn)
                 .Select(x => this.productsService.GetProductModel(x))
                 .ToList();
+
 
             var viewModel = PaginationList<ProductModel>.Create(productModels, page, 12);
             return View(viewModel);
